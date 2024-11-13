660cc6eeabefff67538ad13fdb5b554b69062228
Josh Yim <jyim@lcs.on.ca>
2024-11-13 10:39:01 -0500

    Initial Project organization

---
+++ EnhancedTodoList/Views/LandingView.swift
@@ -0,0 +1,18 @@
+//
+//  LandingView.swift
+//  EnhancedTodoList
+//
+//  Created by junxi Yim on 2024/11/13.
+//
+
+import SwiftUI
+
+struct LandingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandingView()
+}
