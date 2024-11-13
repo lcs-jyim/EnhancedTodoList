59aaf4b9f29eae497b3ab56cff7726ac50ed0043
Josh Yim <jyim@lcs.on.ca>
2024-11-13 10:57:57 -0500

    Made it possible to add new items to the list.

--- EnhancedTodoList/Views/TodoListView.swift
+++ EnhancedTodoList/Views/TodoListView.swift
@@ -8,9 +8,71 @@
 import SwiftUI
 
 struct TodoListView: View {
+    
+    // MARK: Stored properties
+    
+    // Our list of items to complete
+    @State private var items: [TodoItem] = []
+    // The item currently being created
+    @State private var newItemDetails = ""
+    
+    // MARK: Computed properties
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack {
+                HStack {
+                    
+                    TextField("Enter a to-do item", text: $newItemDetails)
+                    
+                    Button("Add") {
+                        addItem()
+                    }
+                 
+                }
+                .padding(20)
+                    
+                    if items.isEmpty {
+                        
+                        ContentUnavailableView(label: {
+                            Label(
+                                "Nothing to do",
+                                systemImage: "powersleep"
+                            )
+                            .foregroundStyle(.green)
+                        }, description: {
+                            Text("To-do items will appear here once you add some.")
+                        })
+                        
+                    } else {
+                        
+                        List(items) { currentItem in
+                            Label {
+                                Text(currentItem.details)
+                            } icon: {
+                                Image(systemName: currentItem.isCompleted ? "checkmark.circle" : "circle")
+                            }
+                        }
+                        
+                
+                }
+            }
+            .navigationTitle("Tasks")
+        }
+        .onAppear {
+            // Populate with example data
+            if items.isEmpty {
+                items.append(contentsOf: exampleData)
+            }
+        }
     }
+    
+    // MARK: Functions
+    func addItem() {
+        let newToDoItem = TodoItem(details: newItemDetails)
+        items.insert(newToDoItem, at: 0)
+        newItemDetails = ""
+    }
+    
 }
 
 #Preview {
