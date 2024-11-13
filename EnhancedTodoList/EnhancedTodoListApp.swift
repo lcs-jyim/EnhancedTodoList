255fee462b28d7a258a8315d637e69bb06602448
Josh Yim <jyim@lcs.on.ca>
2024-11-13 10:25:50 -0500

    Initial Commit

---
+++ EnhancedTodoList/EnhancedTodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnhancedTodoListApp.swift
+//  EnhancedTodoList
+//
+//  Created by junxi Yim on 2024/11/13.
+//
+
+import SwiftUI
+
+@main
+struct EnhancedTodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EnhancedTodoListTests/EnhancedTodoListTests.swift
@@ -0,0 +1,17 @@
+//
+//  EnhancedTodoListTests.swift
+//  EnhancedTodoListTests
+//
+//  Created by junxi Yim on 2024/11/13.
+//
+
+import Testing
+@testable import EnhancedTodoList
+
+struct EnhancedTodoListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
