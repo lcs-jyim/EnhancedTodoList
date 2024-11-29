bf01512820b08365bea0a89a58ab7322a713254b
Josh Yim <jyim@lcs.on.ca>
2024-11-28 21:38:15 -0500

    made the whole stack Observable Finished all 2 exercises

--- EnhancedTodoList/Views/TodoListView.swift
+++ EnhancedTodoList/Views/TodoListView.swift
@@ -15,53 +15,64 @@ struct TodoListView: View {
     @State private var items: [TodoItem] = []
     // The item currently being created
     @State private var newItemDetails = ""
+    @State private var searchText = ""
     
     // MARK: Computed properties
+    var filteredItems: [TodoItem] {
+        if searchText.isEmpty {
+            return items
+        } else {
+            return items.filter { item in
+                item.details.lowercased().contains(searchText.lowercased())
+            }
+        }
+    }
+    
     var body: some View {
         NavigationStack {
             VStack {
                 HStack {
-                    
                     TextField("Enter a to-do item", text: $newItemDetails)
                     
                     Button("Add") {
                         addItem()
                     }
-                 
                 }
                 .padding(20)
-                    
-                    if items.isEmpty {
-                        
-                        ContentUnavailableView(label: {
-                            Label(
-                                "Nothing to do",
-                                systemImage: "powersleep"
-                            )
-                            .foregroundStyle(.green)
-                        }, description: {
-                            Text("To-do items will appear here once you add some.")
-                        })
-                        
-                    } else {
-                        
-                        List(items) { currentItem in
+                
+                if filteredItems.isEmpty {
+                    ContentUnavailableView(label: {
+                        Label(
+                            "Nothing to do",
+                            systemImage: "powersleep"
+                        )
+                        .foregroundStyle(.green)
+                    }, description: {
+                        Text("To-do items will appear here once you add some.")
+                    })
+                } else {
+                    List {
+                        ForEach(filteredItems) { currentItem in
                             Label {
                                 Text(currentItem.details)
                             } icon: {
                                 Image(systemName: currentItem.isCompleted ? "checkmark.circle" : "circle")
+                                    .onTapGesture {
+                                        toggle(item: currentItem)
+                                    }
                             }
                         }
-                        
-                
+                        .onDelete(perform: delItem)
+                    }
                 }
             }
             .navigationTitle("Tasks")
-        }
-        .onAppear {
-            // Populate with example data
-            if items.isEmpty {
-                items.append(contentsOf: exampleData)
+            .searchable(text: $searchText)
+            .onAppear {
+                // Populate with example data
+                if items.isEmpty {
+                    items.append(contentsOf: exampleData)
+                }
             }
         }
     }
@@ -73,6 +84,19 @@ struct TodoListView: View {
         newItemDetails = ""
     }
     
+    func toggle(item: TodoItem) {
+        if item.isCompleted {
+            item.completedOn = nil
+            item.isCompleted = false
+        } else {
+            item.completedOn = Date()
+            item.isCompleted = true
+        }
+    }
+    
+    func delItem(at offsets: IndexSet) {
+        items.remove(atOffsets: offsets)
+    }
 }
 
 #Preview {
